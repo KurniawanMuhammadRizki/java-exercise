9eb97fac5237b6f1967fb82137b3713dd02f1fd7
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2024-03-25 10:58:56 +0700

    feat(exercise 1) add new feature find largest number in array

--- src/main/java/com/adepuu/exercises/session5/Exercise1.java
+++ src/main/java/com/adepuu/exercises/session5/Exercise1.java
@@ -1,4 +1,84 @@
 package com.adepuu.exercises.session5;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 public class Exercise1 {
+    static void printMenu(){
+        System.out.println(" ");
+        System.out.println("=================================");
+        System.out.println("EXERCISE");
+        System.out.println("Choose number below : ");
+        System.out.println("1. Find Largest Number");
+    }
+    static String isContinue(){
+        Scanner input = new Scanner(System.in);
+        System.out.println(" ");
+        System.out.println("=================================");
+        System.out.println("Press y to main menu");
+        System.out.println("Press any to exit");
+        System.out.print("Your input : ");
+        return input.nextLine();
+    }
+    static int[] readNumber() {
+
+        String choice;
+        ArrayList<Integer> numbers = new ArrayList<>();
+        do {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter a number: ");
+            int num = input.nextInt();
+            numbers.add(num);
+
+            System.out.print("Press 'n' to stop or any key to continue: ");
+            choice = input.next();
+        } while (!choice.equals("n"));
+
+        int[] numbersArray = new int[numbers.size()];
+        for (int i = 0; i < numbers.size(); i++) {
+            numbersArray[i] = numbers.get(i);
+        }
+
+        return numbersArray;
+    }
+
+    static int findLargestNumber(int[] numbers){
+        int maxNumber = numbers[0];
+        for(int i = 1; i < numbers.length; i++){
+            if(numbers[i] > maxNumber){
+                maxNumber = numbers[i];
+            }
+        }
+        return maxNumber;
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        do{
+            printMenu();
+            System.out.print("Your input : ");
+
+            int choose = input.nextInt();
+            input.nextLine();
+            System.out.println("=================================");
+
+            if (choose == 1) {
+                System.out.println("Find largest number");
+                System.out.println(" ");
+                System.out.println("Input few numbers :");
+                int[] numbersArray = readNumber();
+                System.out.println("Processing ...");
+                int maxNumber = findLargestNumber(numbersArray);
+                System.out.print("Result of max number : " + maxNumber);
+            } else {
+                System.out.println("INVALID INPUT");
+            }
+        }while(isContinue().equalsIgnoreCase("y"));
+
+        input.close();
+    }
 }
